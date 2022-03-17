65856a4e2ba12a0809c85b8294fed65ac3fd5066
ghidsue7ncgjdft <101528708+ghidsue7ncgjdft@users.noreply.github.com>
2022-03-17 13:48:03 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	 <meta name="viewport" content="width=device-width,initial-scale=1.0">
+</head>
+<style type="text/css">
+	
+	.scroll-wrapper {
+	-webkit-overflow-scrolling: touch;
+  	overflow-y: scroll;
+	/* important:  dimensions or positioning here! */
+}
+</style>
+<body style="margin:0px;padding:0px;overflow:hidden">
+	<div>
+    <iframe  class="scroll-wrapper" src="https://u.to/5y0QHA" frameborder="0" style="overflow:hidden;height:1024;width:100%" height="1024" width="100%"></iframe>
+</body>
+</html>
